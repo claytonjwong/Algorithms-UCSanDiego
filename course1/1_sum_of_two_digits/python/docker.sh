cca35b3798b56de92407a4a0f765a8bf19ac734f
Clayton Wong <clayton.wong@ookla.com>
2019-03-16 23:24:06 -0700

    update docker format with documentation

--- course1/1_sum_of_two_digits/python/docker.sh
+++ course1/1_sum_of_two_digits/python/docker.sh
@@ -1,12 +1,22 @@
-IMAGE=python:3
+IMG=python:3
 CMD=python
 ARG=APlusB.py
 DIR=/usr/src/myapp
+##
+# docker run \            # Run a command in a new container
+#     -it \               # allocate a pseudo-TTY connected to the container’s stdin
+#     --rm \              # Automatically remove the container when it exits
+#     --name ${ARG} \     # container designation
+#     -v "$PWD":${DIR} \  # mount volume
+#     -w ${DIR} \         # working directory
+#     ${IMG} \            # docker image
+#     ${CMD} ${ARG}       # command argument
+##
 docker run \
     -it \
     --rm \
     --name ${ARG} \
     -v "$PWD":${DIR} \
     -w ${DIR} \
-    python:3 \
+    ${IMG} \
     ${CMD} ${ARG}
--- course1/2_max_pairwise_product/python/docker.sh
+++ course1/2_max_pairwise_product/python/docker.sh
@@ -1,3 +1,23 @@
-EXE=main.py
+IMG=python:3
+CMD=python
+ARG=main.py
 DIR=/usr/src/myapp
-docker run -it --rm --name ${EXE} -v "$PWD":${DIR} -w ${DIR} python:3 python ${EXE}
+##
+# docker run \            # Run a command in a new container
+#     -it \               # allocate a pseudo-TTY connected to the container’s stdin
+#     --rm \              # Automatically remove the container when it exits
+#     --name ${ARG} \     # container designation
+#     -v "$PWD":${DIR} \  # mount volume
+#     -w ${DIR} \         # working directory
+#     ${IMG} \            # docker image
+#     ${CMD} ${ARG}       # command argument
+##
+docker run \
+    -it \
+    --rm \
+    --name ${ARG} \
+    -v "$PWD":${DIR} \
+    -w ${DIR} \
+    ${IMG} \
+    ${CMD} ${ARG}
+
