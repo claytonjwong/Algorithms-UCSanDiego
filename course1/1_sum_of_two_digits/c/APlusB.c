d5b20f5f90aea6630cb8fde332ef7770de273854
Clayton Wong <clayton.wong@ookla.com>
2019-03-16 22:34:15 -0700

    minor whitespace

--- course1/1_sum_of_two_digits/c/APlusB.c
+++ course1/1_sum_of_two_digits/c/APlusB.c
@@ -15,6 +15,6 @@ int sum_of_two_digits( int first, int second ){
 int main(){
     int a = 0, b = 0;
     scanf( "%d%d", &a, &b );
-    printf( "%d\n", sum_of_two_digits( a, b ) );
+    printf( "%d\n", sum_of_two_digits( a, b ));
     return 0;
 }
