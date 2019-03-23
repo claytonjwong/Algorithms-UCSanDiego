7130885de47cf54d2aa892bd551578db6e1c0e6b
Clayton Wong <clayton.wong@ookla.com>
2019-03-23 08:55:15 -0700

    whitespace

--- course1/week2/6_last_digit_of_the_sum_of_fibonacci_numbers/cpp/main.cpp
+++ course1/week2/6_last_digit_of_the_sum_of_fibonacci_numbers/cpp/main.cpp
@@ -30,9 +30,9 @@ public:
         Type i = 3; // start looking for the pisano period from i=3 to ignore the first 0,1 sequence at dp[ 0 ] and dp[ 1 ]
         for(; i <= N && ! ( dp[ i-2 ] == 0 && dp[ i-1 ] == 1 ); ++i )
             dp[ i ] = ( dp[ i-2 ] + dp[ i-1 ] ) % M;
-        Type P = i-2;                          // (P)isano period
-        return( N <= i-1 )? sum(dp, N,     M)  // case 1) (P)isano period NOT reached, return the sum % M of N-th fibonacci number
-                          : sum(dp, N % P, M); // case 2) (P)isano period reached, return the sum % M of (N mod P)-th fibonacci number
+        Type P = i-2;                            // (P)isano period
+        return( N <= i-1 )? sum( dp, N,     M )  // case 1) (P)isano period NOT reached, return the sum % M of N-th fibonacci number
+                          : sum( dp, N % P, M ); // case 2) (P)isano period reached, return the sum % M of (N mod P)-th fibonacci number
     }
 };
 
