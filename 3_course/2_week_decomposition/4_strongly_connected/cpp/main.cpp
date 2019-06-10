06b9a0c159bd188f5e88db3f9c51c07ed15777e9
Clayton Wong <clayton.wong@ookla.com>
2019-06-10 08:59:13 -0700

    bug fix for topo sort which includes a cycle ( i.e. BFS pruning cannot topo sort, instead DFS is required )

--- 3_course/2_week_decomposition/4_strongly_connected/cpp/main.cpp
+++ 3_course/2_week_decomposition/4_strongly_connected/cpp/main.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
-#include <set>
 #include <vector>
-#include <queue>
-#include <sstream>
-#include <fstream>
 
 using namespace std;
-using Vertex = size_t;
-using AdjacencyList = unordered_set< Vertex >;
-using Graph = unordered_map< Vertex, AdjacencyList >;
+using Vertex = int;
+using Graph = unordered_map< Vertex, unordered_set< Vertex > >;
 using Seen = unordered_set< Vertex >;
-using ConnectedComponents = vector< vector< Vertex > >;
+using Components = vector< vector< Vertex > >;
 using Sorted = vector< Vertex >;
 using Stack = vector< Vertex >;
 using Path = vector< Vertex >;
-using InDegree = vector< Vertex >;
-using Queue = queue< Vertex >;
 
 class Solution {
 public:
-    ConnectedComponents getSCC( Graph& G, int N, ConnectedComponents CC={}, Stack S={}, Seen seen={} ){
-        InDegree D( N+1 );
-        auto L = topo_sort( reverse( G, D ), N, D );
+    Components getSCC( Graph& G, int N, Components SCC={}, Stack S={}, Seen seen={} ){
+        auto L = topo_sort( reverse( G ), N );
         for( auto vertex: L ){
-            if( seen.insert( vertex ).second )
-                S.push_back( vertex );
+            if( ! seen.insert( vertex ).second )
+                continue;
+            S.push_back( vertex );
             Path P;
             while( ! S.empty() ){
                 auto u = S.back(); S.pop_back();
@@ -35,37 +28,37 @@ public:
                     if( seen.insert( v ).second )
                         S.push_back( v );
             }
-            if( ! P.empty() )
-                CC.emplace_back( P );
+            SCC.emplace_back( P );
         }
-        return CC;
+        return SCC;
     }
 private:
-    Graph reverse( Graph& G, InDegree& D ){
-        Graph R( G );        // (R)eversed (G)raph: keep G's vertex keys ( pair.first ),
+    Graph reverse( Graph& G ){
+        Graph R{ G };        // (R)eversed (G)raph: keep G's vertex keys ( pair.first ),
         for( auto& pair: R ) // but clear G's adjacency lists ( pair.second )
-            pair.second={};
+            pair.second = {};
         for( auto& pair: G ){
-            auto u{ pair.first };
-            for( auto v: G[ u ] ){  // u -> v
+            auto u = pair.first;
+            for( auto v: G[ u ] )   // u -> v
                 R[ v ].insert( u ); // v -> u
-                ++D[ u ];
-            }
         }
         return R;
     }
-    Sorted topo_sort( Graph&& G, int N, InDegree& D, Queue q=Queue(), Sorted S={} ){
-        for( auto i{ 1 }; i <= N; ++i )
-            if( D[ i ] == 0 )
-                q.push( i );
-        for( Vertex u{ 0 }, v{ 0 }; ! q.empty(); ){
-            u = q.front(), q.pop();
-            S.push_back( u );
-            for( auto v: G[ u ] )
-                if( --D[ v ] == 0 )
-                    q.push( v );
+
+    Sorted topo_sort( Graph&& G, int N, Seen seen={} ){
+        Sorted S( N );
+        for( auto i{ 1 }, K{ N-1 }; i <= N; ++i ){
+            if( seen.insert( i ).second )
+                go( S, move( G ), i, K, seen );
         }
         return S;
+    }
+
+    void go( Sorted& S, Graph&& G, Vertex u, int& K, Seen& seen ){
+        for( auto v: G[ u ] )
+            if( seen.insert( v ).second )
+                go( S, move( G ), v, K, seen );
+        S[ K-- ] = u;
     }
 };
 
@@ -74,7 +67,7 @@ int main() {
     Graph G;
     auto N{ 0 }, M{ 0 }; cin >> N >> M;
     for( Vertex u{ 0 }, v{ 0 }; M-- && cin >> u >> v; G[ u ].insert( v ) );
-    auto CC = s.getSCC( G, N );
-    cout << CC.size() << endl;
+    auto SCC = s.getSCC( G, N );
+    cout << SCC.size() << endl;
     return 0;
 }
